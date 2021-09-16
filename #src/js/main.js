a4248b390576021870e8615234946eb6a7bedf61
ZorikovPasha <zorikov.pasha@mail.ru>
2021-09-16 09:30:22 +0300

    fixing code

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,11 +1,17 @@
 $(function(){
 
+  // Выпадание и закрытие фильтра в боковой панели каталога
+
   $('.aside-dropdown__title').on('click', function() {
     $(this).toggleClass('to-open')
     $(this).siblings('.aside-dropdown__box').slideToggle()
   })
 
+  // закрытие последних фильтров при загрузке
+
   $('.aside-dropdown__box').slideUp(0);
+
+  // открытие каталога на мобильных экранах
 
   $('.aside-toggle').on('click', function() {
     $('.aside-catalog').toggleClass('opened')
