f40b482ca3fa472889b352318119c74257270f0b
ZorikovPasha <zorikov.pasha@mail.ru>
2021-09-17 14:37:08 +0300

    fixed popup

--- #src/js/my-popup.js
+++ #src/js/my-popup.js
@@ -1,31 +1,31 @@
 $(function() {
   $(".header__btn, .footer__btn").on("click", function () {
-    $(".popup").fadeIn();
-    $("body").addClass("lock");
+    $(".popup").addClass('shown')
+    $("body").addClass("lock")
   });
   
   // Клик по ссылке "Закрыть".
   $(".popup__close").on("click", function () {
-    $(this).closest(".popup").fadeOut();
-    $("body").removeClass("lock");
+    $(this).closest(".popup").removeClass('shown')
+    $("body").removeClass("lock")
   });
   
   // Закрытие по клавише Esc.
   $(document).on("keydown", function (e) {
     if (e.key === "Escape") {
-      $(".popup").fadeOut();
-      $(".popup-message").fadeOut();
+      $(".popup").removeClass('shown')
+      $(".popup-message").removeClass('shown')
       $("body").removeClass("lock");
     }
   });
   
   $(".popup__btn").on("click", function () {
-    $(this).closest(".popup").fadeOut();
-    $(".popup-message").fadeIn();
+    $(this).closest(".popup").removeClass('shown');
+    $(".popup-message").addClass('shown');
   
     // Клик по крестику или по кнопке вернуться.
     $(".popup-message__close, .popup-message__btn").on("click", function () {
-      $(this).closest(".popup-message").fadeOut();
+      $(this).closest(".popup-message").removeClass('shown');
       $("body").removeClass("lock");
     });
   });
